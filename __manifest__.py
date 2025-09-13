420947868f3605dc945bc951975f04c29b718c35
mah007 <7385542+mah007@users.noreply.github.com>
2025-09-13 15:07:18 -0400

    Initial commit: Odoo 18.0 compatible DietFacts module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'DietFacts',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Adds nutrition information to products',
+    'description': 'Adds nutrition information to products',
+    'author': 'Greg Moss, OdooClass.com',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'dietfacts_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
